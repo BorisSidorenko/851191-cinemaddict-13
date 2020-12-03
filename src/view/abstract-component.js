d865fb45dbc6c4367b7d6a17fe0de3273322f82c
Boris <boris.m.sidorenko@gmail.com>
2020-12-03 17:49:03 +0300

    Вынес общие методы в абстрактный класс

--- src/view/abstract-component.js
+++ src/view/abstract-component.js
@@ -1,7 +1,27 @@
+import {createElement} from "../utils";
+
 export default class AbstractComponent {
   constructor() {
     if (new.target === AbstractComponent) {
       throw new Error(`Can't instantiate AbstractComponent, only concrete one.`);
     }
+
+    this._element = null;
+  }
+
+  getTemplate() {
+    throw new Error(`Abstract method is not implemented: getTemplate`);
+  }
+
+  get element() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
   }
 }
