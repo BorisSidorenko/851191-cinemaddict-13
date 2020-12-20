c6b698be51712c19ed6f43fe7f85348f437de498
Boris <boris.m.sidorenko@gmail.com>
2020-12-20 16:45:31 +0300

    убрал лишние хендлеры

--- src/view/film-popup-controls/film-popup-controls.js
+++ src/view/film-popup-controls/film-popup-controls.js
@@ -15,56 +15,18 @@ export default class FilmPopupControls extends AbstractView {
     return createFilmPopupControlsTemplate(this._card);
   }
 
-  _addToHistoryClickHandler(evt) {
-    evt.preventDefault();
-    this._callback.addToHistoryClick();
-  }
-
-  setAddToHistoryClickHandler(callback) {
-    this._callback.addToHistoryClick = callback;
-    this.element.querySelector(`#watched`).addEventListener(`click`, this._addToHistoryClickHandler);
-  }
-
-  clearAddToHistoryClickHandler() {
-    this._callback.addToHistoryClick = null;
-    this.element.querySelector(`#watched`).removeEventListener(`click`, this._addToHistoryClickHandler);
-  }
-
-  _addToWatchListClickHandler(evt) {
-    evt.preventDefault();
-    this._callback.addToWatchListClick();
-  }
-
-  setAddToWatchListClickHandler(callback) {
-    this._callback.addToWatchListClick = callback;
-    this.element.querySelector(`#watchlist`).addEventListener(`click`, this._addToWatchListClickHandler);
-  }
-
-  clearAddToWatchListClickHandler() {
-    this._callback.addToWatchListClick = null;
-    this.element.querySelector(`#watchlist`).removeEventListener(`click`, this._addToWatchListClickHandler);
-  }
-
-  _addToFavoriteClickHandler(evt) {
-    evt.preventDefault();
-    this._callback.addToFavoriteClick();
-  }
-
-  setAddToFavoriteClickHandler(callback) {
-    this._callback.addToFavoriteClick = callback;
-    this.element.querySelector(`#favorite`).addEventListener(`click`, this._addToFavoriteClickHandler);
-  }
-
-  clearAddToFavoriteClickHandler() {
-    this._callback.addToFavoriteClick = null;
-    this.element.querySelector(`#favorite`).removeEventListener(`click`, this._addToFavoriteClickHandler);
-  }
-
   setClickHandlers(callback) {
     this._callback.popupControlsClick = callback;
     this.element.querySelector(`#watchlist`).addEventListener(`click`, this._handleControlsClick);
     this.element.querySelector(`#watched`).addEventListener(`click`, this._handleControlsClick);
     this.element.querySelector(`#favorite`).addEventListener(`click`, this._handleControlsClick);
+  }
+
+  clearClickHandlers() {
+    this._callback.popupControlsClick = null;
+    this.element.querySelector(`#watchlist`).removeEventListener(`click`, this._handleControlsClick);
+    this.element.querySelector(`#watched`).removeEventListener(`click`, this._handleControlsClick);
+    this.element.querySelector(`#favorite`).removeEventListener(`click`, this._handleControlsClick);
   }
 
   _handleControlsClick(evt) {
