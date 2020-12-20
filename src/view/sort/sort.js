27cb219fc7b424377f4027b5396bc7630b2e3798
Boris <boris.m.sidorenko@gmail.com>
2020-12-20 19:47:34 +0300

    добавил подписку на изменение типа сортировки

--- src/view/sort/sort.js
+++ src/view/sort/sort.js
@@ -2,7 +2,26 @@ import AbstractView from "../abstract-component";
 import {createSortTemplate} from "../sort/sort-template";
 
 export default class Sort extends AbstractView {
+  constructor() {
+    super();
+    this._handleSortTypeChange = this._handleSortTypeChange.bind(this);
+  }
+
   getTemplate() {
     return createSortTemplate();
+  }
+
+  _handleSortTypeChange(evt) {
+    const sortType = evt.target.dataset.sortType;
+
+    if (sortType) {
+      evt.preventDefault();
+      this._callback.sortTypeChange(sortType);
+    }
+  }
+
+  setSortTypeChangeHandler(callback) {
+    this._callback.sortTypeChange = callback;
+    this.element.addEventListener(`click`, this._handleSortTypeChange);
   }
 }
