df7bdde16be08711c4bf20d03ef0b2aeb855e9c4
Boris <boris.m.sidorenko@gmail.com>
2021-01-06 19:37:58 +0300

    добавил компонент статистики

---
+++ src/view/stats/stats.js
@@ -0,0 +1,8 @@
+import AbstractView from "../abstract-component";
+import {createStatsTemplate} from "../stats/stats-template";
+
+export default class Stats  extends AbstractView {
+  getTemplate() {
+    return createStatsTemplate();
+  }
+}
