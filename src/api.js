725443ed2820fb993cf7cda4649fb13e7d086d25
Boris <boris.m.sidorenko@gmail.com>
2021-01-14 13:31:19 +0300

    перенёс методы адаптера в модель фильмов

--- src/api.js
+++ src/api.js
@@ -1,80 +1,16 @@
+import FilmsModel from "../src/model/films";
 import {Method, Url} from "../src/utils/constants";
 
 export default class Api {
   constructor(endPoint, auth) {
     this._endPoint = endPoint;
     this._auth = auth;
-
-    this._adaptFilmToClient = this._adaptFilmToClient.bind(this);
   }
 
   getFilms() {
     return this._load({url: Url.FILMS})
       .then((response) => response.json())
-      .then((films) => films.map(this._adaptFilmToClient));
-  }
-
-  _adaptFilmToClient(film) {
-    const adaptedFilm = Object.assign(
-        {},
-        film,
-        {
-          filmInfo: this._adaptFilmInfoToClient(film.film_info)
-        },
-        {
-          userDetails: this._adaptUserDetailsToClient(film.user_details)
-        }
-    );
-
-    delete adaptedFilm.film_info;
-    delete adaptedFilm.user_details;
-
-    return adaptedFilm;
-  }
-
-  _adaptFilmInfoToClient(filmInfo) {
-    const adaptedRelease = Object.assign(
-        {},
-        filmInfo.release,
-        {
-          releaseCountry: filmInfo.release.release_country
-        }
-    );
-
-    delete adaptedRelease.release_country;
-
-    const adaptedFilmInfo = Object.assign(
-        {},
-        filmInfo,
-        {
-          ageRating: filmInfo.age_rating,
-          alternativeTitle: filmInfo.alternative_title,
-          totalRating: filmInfo.total_rating,
-          release: adaptedRelease
-        }
-    );
-
-    delete adaptedFilmInfo.age_rating;
-    delete adaptedFilmInfo.alternative_title;
-    delete adaptedFilmInfo.total_rating;
-
-    return adaptedFilmInfo;
-  }
-
-  _adaptUserDetailsToClient(userDetails) {
-    const adaptedUserDetails = Object.assign(
-        {},
-        userDetails,
-        {
-          alreadyWatched: userDetails.already_watched,
-          watchingDate: userDetails.watching_date
-        }
-    );
-
-    delete adaptedUserDetails.already_watched;
-    delete adaptedUserDetails.watching_date;
-
-    return adaptedUserDetails;
+      .then((films) => films.map(FilmsModel.adaptFilmToClient));
   }
 
   getFilmComments({id}) {
