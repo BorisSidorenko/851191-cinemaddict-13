85bcd9e39692f476a7eb52fe600a5d03a0a7d24a
Boris <boris.m.sidorenko@gmail.com>
2021-01-23 16:15:51 +0300

    переименовал вью согласно названию файла

--- src/view/films-list-empty/films-list-empty.js
+++ src/view/films-list-empty/films-list-empty.js
@@ -1,7 +1,7 @@
 import AbstractView from "../abstract-component";
 import {createFilmsListEmptyTemplate} from "../films-list-empty/films-list-empty-template";
 
-export default class EmptyFilmsList extends AbstractView {
+export default class FilmsListEmpty extends AbstractView {
   getTemplate() {
     return createFilmsListEmptyTemplate();
   }
